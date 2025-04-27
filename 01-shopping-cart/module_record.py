71af03a4c942a69babed0108be0675c240a70d86
Ronald Acosta <ronald.acostaalva@gmail.com>
2025-04-27 15:38:17 -0500

    upd try/except module_record

--- 01-shopping-cart/module_record.py
+++ 01-shopping-cart/module_record.py
@@ -10,7 +10,13 @@ def save_purchase(products: List[Dict[str, any]], total: float) -> None:
         f"{p['name']} (x{p['quantity']})" 
         for p in products
     )
-    
-    with open(HISTORICAL_ARCHIVE, "a", newline="", encoding="utf-8") as file:
-        writer = csv.writer(file)
-        writer.writerow([purchase_date, products_str, f"S/{total:.2f}"])
+
+    try:
+        with open(HISTORICAL_ARCHIVE, "a", newline="", encoding="utf-8") as file:
+            writer = csv.writer(file)
+            writer.writerow([purchase_date, products_str, f"S/{total:.2f}"])
+        
+    except FileNotFoundError:
+        print("¡The file isn't found!")
+    except Exception as e:
+        print("An error occurred:", e)
